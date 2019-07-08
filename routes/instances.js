b5cbe18c14bffdedf8f272a60fc66736ba072c90
ShadowBeast <ShadovvBeast@gmail.com>
2019-07-09 00:18:01 +0300

    Removing ability to run contained in docker

--- routes/instances.js
+++ routes/instances.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Docker = require('dockerode');
 const jwt = require('jsonwebtoken');
-const isDocker = require('is-docker');
-//const docker = new Docker({host: '127.0.0.1', port: 2375});
 const conf = require('../config');
+const docker = new Docker({host: conf.docker_host, port: 2375});
+
+// Initialize Docker
+// Remove all containers
+docker.listContainers({all: true}).then(containers => containers.forEach((containerInfo) => docker.getContainer(containerInfo.Id).remove({force: true})));
+// Pull images
+let pullImage = imagename => docker.pull(imagename, (error, stream) => error ? console.error(error) : console.log(`Successfully pulled ${imagename}`));
+pullImage('mysql');
+pullImage('redis');
+pullImage('jupyter/all-spark-notebook');
 const Lab = require('../models/Lab');
-const verifyToken = (req, res, next) => req.headers['token'] ? jwt.verify(req.headers['token'], conf.jwt_secret, (err, authData) => err ? res.sendStatus(403) : next()) : res.sendStatus(403) // Verify the JWT token
+
+// Verify the JWT token
+const verifyToken = (req, res, next) => req.headers['token'] ? jwt.verify(req.headers['token'], conf.jwt_secret, (err, authData) => err ? res.sendStatus(403) : next()) : res.sendStatus(403) 
+
 // @route GET /instances
 // @desc Get all lab instances (running docker containers)
 router.get('/', verifyToken, (req, res) => {
@@ -37,12 +48,11 @@ router.post('/start', verifyToken, (req, res) => {
         }
         else 
             res.json({message: "Lab not found"});
-
     });
 });
 
 // @route POST /instances/stop
 // @desc Stop an instance (removes the docker container)
-router.post('/stop', verifyToken, (req, res) => docker.getContainer(req.body.instanceId).remove({force: true}));
+router.post('/stop', verifyToken, (req, res) => docker.getContainer(req.body.instanceId).remove({force: true}).then(() => res.json('Instance removed')));
 
 module.exports = router;
